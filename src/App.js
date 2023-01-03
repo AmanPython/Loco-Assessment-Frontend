30cf5321a5dc55cc379f875e02bc006042864dca
Aman <amangora1999@gmail.com>
2023-01-03 09:01:36 -0500

    CRED APP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ListTransaction from "./component/user/ListTransaction";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="col-md-6">
+          <h1 className="text-center" style={style}>Transaction Application (Loco Assignment)</h1>
+          <ListTransaction/>
+        </div>
+      </Router>
     </div>
   );
+}
+
+const style = {
+  color: 'black',
+  margin: '10px'
 }
 
 export default App;
